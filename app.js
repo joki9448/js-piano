8ba7d6f6be9257f6d28c4ced72b8aa603ca782a7
John Kim <joki9448@gmail.com>
2023-10-03 17:33:25 -0400

    fixed black key positioning

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-document.querySelectorAll(".key");JSON
+const pianoKeys = document.querySelectorAll(".key"); 
 
 function playSound(newUrl) {
+    console.log(newUrl)
     new Audio(newUrl).play()
 }
 
 pianoKeys.forEach((pianoKey, i) => {
     const number = i < 9 ? '0' + (i + 1) : (i + 1)
     const newUrl = '24-piano-keys/key' + number + '.mp3'
-    console.log(newUrl)
-    pianoKey.addEventListener('click', playSound(newUrl))
+    pianoKey.addEventListener('click', () => playSound(newUrl))
 })
