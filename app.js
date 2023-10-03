8a3976dbfb53dba07b042d6b01a9594d32d798d6
John Kim <joki9448@gmail.com>
2023-10-03 17:22:50 -0400

    more js logic

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 document.querySelectorAll(".key");JSON
 
-function playSound() {
-    new Audio().play()
+function playSound(newUrl) {
+    new Audio(newUrl).play()
 }
 
 pianoKeys.forEach((pianoKey, i) => {
+    const number = i < 9 ? '0' + (i + 1) : (i + 1)
     const newUrl = '24-piano-keys/key' + number + '.mp3'
+    console.log(newUrl)
     pianoKey.addEventListener('click', playSound(newUrl))
 })
